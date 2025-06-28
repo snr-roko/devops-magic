2e1b3be4dd05394db18332c671cfeefe6e96e94b
snr-roko <rbbagyei@outlook.com>
2025-06-28 08:53:22 +0000

    Add Internet Gateway and Elastic IP resources to Terraform configuration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,5 +6,22 @@ resource "aws_vpc" "production_vpc" {
   }
 }
 
+# Creating the Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.production_vpc.id
+}
 
+# Creating an Elastic IP to associate with NAT Gateway
+resource "aws_eip" "nat_eip" {
+  depends_on = [ aws_internet_gateway.igw ]
+}
 
+/*# Creating the NAT Gateway
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public_subnet1.id
+  tags = {
+    Name = "NAT Gateway"
+  }
+}
+*/
